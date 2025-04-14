a66dbbce86836184cf1c4bcf1dff8881f1d34673
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-04-14 01:31:34 -0700

    include Throwaway class for developing pipeline-manager module

--- .dagger/src/throwaway/main.py
+++ .dagger/src/throwaway/main.py
@@ -1,22 +1,59 @@
 import dagger
-from dagger import dag, function, object_type
-
+from dagger import dag, function, object_type, Container, DefaultPath
+from typing import Annotated
+import asyncio
 
 @object_type
-class Throwaway:
+class Throwaway: # this will be our pipeline-manager module
+    
+    
     @function
-    def container_echo(self, string_arg: str) -> dagger.Container:
-        """Returns a container that echoes whatever string argument is provided"""
-        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
+    async def publish(self) -> None:
+        print("Publishing...")
+        tag = await self.create_tag()
+        container = await self.build_image()
+
+        return await container.publish(
+            f"some_registry/some_repo:{tag}"
+        )
 
     @function
-    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
-        return await (
-            dag.container()
-            .from_("alpine:latest")
-            .with_mounted_directory("/mnt", directory_arg)
-            .with_workdir("/mnt")
-            .with_exec(["grep", "-R", pattern, "."])
-            .stdout()
-        )
+    def build_image(self,
+            source: Annotated[
+            dagger.Directory,
+            DefaultPath("./")
+        ],
+        ) -> Container:
+
+        self.run_tests()
+        """Builds the image for the build"""
+        return source.docker_build()
+
+    @function
+    async def run_tests(self) -> None:
+        """Returns a container that echoes whatever string argument is provided"""
+        return None
+
+    @function
+    async def create_tag(self) -> None:
+        """Creates the tag for the build"""
+
+        version_task = self.semantic_release()
+        environment_task = self.determine_environment()
+
+        version, environment = await asyncio.gather(version_task, environment_task)
+
+        some_format_we_agreed_on = f"{version}-{environment}"
+
+        return some_format_we_agreed_on
+
+    
+    @function
+    def semantic_release(self) -> None:
+        """Returns a container that echoes whatever string argument is provided"""
+        return None
+    
+    @function
+    def determine_environment(self) -> None:
+        """Returns a container that echoes whatever string argument is provided"""
+        return None
