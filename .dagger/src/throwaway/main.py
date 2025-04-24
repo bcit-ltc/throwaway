b8f72ae3b62a2ee0c48efadb5bcd718d6b3e0b1c
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-04-24 08:27:03 -0700

    fix: pipeline manager run function

--- .dagger/src/throwaway/main.py
+++ .dagger/src/throwaway/main.py
@@ -1,59 +1,51 @@
 import dagger
-from dagger import dag, function, object_type, Container, DefaultPath
+from dagger import dag, function, object_type, Container, DefaultPath, Directory, Secret, Doc, Enum, EnvVariable, enum_type
 from typing import Annotated
-import asyncio
+# import asyncio
+import json
+
+@enum_type
+class Environment(Enum):
+    STABLE = "stable"
+    REVIEW = "review"
+    LATEST = "latest"
+    LOCAL = "local"
+    NONE = "none"
+
+MAIN_BRANCH = "main"
 
 @object_type
 class Throwaway: # this will be our pipeline-manager module
+    environment = Environment.NONE
+    semantic_release_result = None
+
+    @function
+    async def run(self,
+                source: Annotated[Directory, Doc("Source directory"), DefaultPath(".")], # source directory
+                github_token: Annotated[Secret, Doc("Github Token")] | None,
+                username: Annotated[str, Doc("Github Username")] | None,  # GitHub username
+                branch: Annotated[str, Doc("Current Branch")] | None,  # Current branch
+                commit_hash: Annotated[str, Doc("Current Commit Hash")] | None,  # Current commit hash
+                  ) -> str:
+
+        # Check for GitHub token
+        if github_token is None:
+            environment = Environment.LOCAL
+        else:
+            if branch == MAIN_BRANCH:
+                # semantic_release = await self.semantic_release()
+                semantic_release_result = '{ "next_release": null, "last_release": "1.0.1"}'
+
+                # Convert JSON String to Python
+                semantic_release_result = json.loads(semantic_release_result)
+                print(semantic_release_result['next_release'])
+
+                if semantic_release_result['next_release']:
+                    environment = Environment.STABLE
+                else:
+                    environment = Environment.LATEST
+            else:
+                environment = Environment.REVIEW
     
-    
-    @function
-    async def publish(self) -> None:
-        print("Publishing...")
-        tag = await self.create_tag()
-        container = await self.build_image()
+        return environment
 
-        return await container.publish(
-            f"some_registry/some_repo:{tag}"
-        )
-
-    @function
-    def build_image(self,
-            source: Annotated[
-            dagger.Directory,
-            DefaultPath("./")
-        ],
-        ) -> Container:
-
-        self.run_tests()
-        """Builds the image for the build"""
-        return source.docker_build()
-
-    @function
-    async def run_tests(self) -> None:
-        """Returns a container that echoes whatever string argument is provided"""
-        return None
-
-    @function
-    async def create_tag(self) -> None:
-        """Creates the tag for the build"""
-
-        version_task = self.semantic_release()
-        environment_task = self.determine_environment()
-
-        version, environment = await asyncio.gather(version_task, environment_task)
-
-        some_format_we_agreed_on = f"{version}-{environment}"
-
-        return some_format_we_agreed_on
-
-    
-    @function
-    def semantic_release(self) -> None:
-        """Returns a container that echoes whatever string argument is provided"""
-        return None
-    
-    @function
-    def determine_environment(self) -> None:
-        """Returns a container that echoes whatever string argument is provided"""
-        return None
