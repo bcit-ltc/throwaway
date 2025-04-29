8c092edf8a6a7da1a2f2b8d83cea8b9fa7eec6c8
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-04-28 22:42:15 -0700

    install pipeline manager

--- .dagger/src/throwaway/main.py
+++ .dagger/src/throwaway/main.py
@@ -16,36 +16,15 @@ MAIN_BRANCH = "main"
 
 @object_type
 class Throwaway: # this will be our pipeline-manager module
-    environment = Environment.NONE
-    semantic_release_result = None
 
     @function
     async def run(self,
                 source: Annotated[Directory, Doc("Source directory"), DefaultPath(".")], # source directory
-                github_token: Annotated[Secret, Doc("Github Token")] | None,
-                username: Annotated[str, Doc("Github Username")] | None,  # GitHub username
-                branch: Annotated[str, Doc("Current Branch")] | None,  # Current branch
-                commit_hash: Annotated[str, Doc("Current Commit Hash")] | None,  # Current commit hash
                   ) -> str:
 
-        # Check for GitHub token
-        if github_token is None:
-            environment = Environment.LOCAL
-        else:
-            if branch == MAIN_BRANCH:
-                # semantic_release = await self.semantic_release()
-                semantic_release_result = '{ "next_release": null, "last_release": "1.0.1"}'
+        print(dir(dag.pipeline_manager))
 
-                # Convert JSON String to Python
-                semantic_release_result = json.loads(semantic_release_result)
-                print(semantic_release_result['next_release'])
+        
 
-                if semantic_release_result['next_release']:
-                    environment = Environment.STABLE
-                else:
-                    environment = Environment.LATEST
-            else:
-                environment = Environment.REVIEW
-    
-        return environment
+        return await dag.pipeline_manager().run()
 
